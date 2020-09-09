0ef9a67e77a1ebe1f7c7e3689d4aa5378c05a354
Eddie Moser <moser.eddie@gmail.com>
2020-09-09 01:21:15 -0500

    testing

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -553,7 +553,9 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 406 }
+//#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 445 } //Default Steps for stock Sidewinder
+//#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 406 } // Steps for E3D Hermera
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 1200 } // Steps for Slice Engineering Mosquito
 
 /**
  * Default Max Feed Rate (mm/s)
