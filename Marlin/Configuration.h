7e04120df495e843ccc43f39b944102a65af1d38
Eddie Moser <moser.eddie@gmail.com>
2020-09-09 00:48:27 -0500

    testing

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -592,8 +592,8 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-//#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 1200, 445 } Starting point for Mosquito & Bondtech BMG
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 445 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 1200, 445 } //Starting point for Mosquito & Bondtech BMG
+//#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 445 }
 
 /**
  * Default Max Feed Rate (mm/s)
